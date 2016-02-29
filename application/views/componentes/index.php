92b1e317666e2d85c3c6e942a028faa7f05e004f
JennS10 <jennserrano_10@hotmail.com>
2016-02-29 00:31:37 -0600

    Creacion de Componenetes para vehiculo

---
+++ application/views/componentes/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+
+	</title>
+	<?php
+		// estilos universales
+			$this->load->view("parts/estilos.php");
+	?>
+	<!-- estilos personalizados -->
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/css/componentesvehiculos/style.css") ?>>
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/css/componevehiculos/media.css") ?>>
+</head>
+<body>
+<div id="centrado">
+        <header>
+            <hgroup>
+                <h2>Componentes de Vehiculo</h1>
+                <h1>Breve descripcion de los componentes del Vehiculo</h2>
+            </hgroup>
+        </header>
+
+
+</body>
+</html>
