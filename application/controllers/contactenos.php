e8bbdc1d2eb3450da6a688de13f4b788acc06988
JennS10 <jennserrano_10@hotmail.com>
2016-03-02 04:08:14 -0600

    formulario de contactenos listo PERO FEO :-1:

--- application/controllers/contactenos.php
+++ application/controllers/contactenos.php
@@ -1,1 +1,16 @@
-contactenos.php
+<?php 
+
+class contactenos extends CI_Controller {
+	// metodos 
+		function __construct()
+		{
+			parent::__construct();
+		}
+		// URL
+		// localhost://drivecontrol/index.php/contactenos/index/ 
+		public function index(){
+			$this->load->view("contactenos/index.php");
+
+			}
+
+		}
--- application/controllers/perfil.php
+++ application/controllers/perfil.php
@@ -1,1 +1,16 @@
-perfil.php
+<?php 
+
+class perfil extends CI_Controller {
+	// metodos 
+		function __construct()
+		{
+			parent::__construct();
+		}
+		// URL
+		// localhost://drivecontrol/index.php/componentes/index/ 
+		public function index(){
+			$this->load->view("perfil/index.php");
+
+			}
+
+		}
