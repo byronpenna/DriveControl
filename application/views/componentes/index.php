324ad84a083cf7b1e74f96bece7b2337ecd0f3a8
JennS10 <jennserrano_10@hotmail.com>
2016-03-02 01:14:30 -0600

    Agregue detalles de los componentes del vehiculo

--- application/views/componentes/index.php
+++ application/views/componentes/index.php
@@ -11,15 +11,179 @@
 	<!-- estilos personalizados -->
 		<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/css/componentes/style.css") ?>>
 		<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/css/componentes/media.css") ?>>
-</head>
+</head
 <body>
 <div id="centrado">
+
         <header>
-            <hgroup>
-                <h2>Componentes de Vehiculo</h1>
-                <h1>Breve descripcion de los componentes del Vehiculo</h2>
-            </hgroup>
+               
+               <h1>Componestes de Vehiculo</h1>
+                <h3>Breve descripcion de los componentes del Vehiculo</h3>
         </header>
+
+<h2><li> Aceite de motor </li></h2>
+
+<p>
+Aceite que se utiliza para lubricar los motores de combustión interna. Su propósito principal es lubricar las partes móviles reduciendo la fricción. Además de lubricar el aceite también limpia, inhibe la corrosión y reduce la temperatura del motor transmitiendo el calor lejos de las partes móviles para disiparlo.
+</p>
+
+<h2><li> Alternador del motor </li></h2>
+
+<P>  
+Es una máquina eléctrica, capaz de generar energía eléctrica a partir de energía mecánica, generalmente obtenida por un mecanismo de arrastre desde un motor de combustión interna, tanto alternativo, como turbina de gas o Wankel. La corriente eléctrica producida es corriente alterna, no necesita sistema de regulación de la intensidad o disyuntor como la dinamo. Sin embargo sí necesita un dispositivo de regulación del voltaje y de rectificación, ya que la corriente usada por los sistemas es normalmente continua y obtenida desde una batería o acumulador
+</p>
+
+<h2><li> Amortiguador </li></h2>
+
+<P> 
+Es un dispositivo construido con un eje cromado y dos tubos de acero (uno dentro del otro). El tubo exterior se denomina tubo de reserva (lleno de aceite). El interno, tubo de compresión. En un extremo, el eje de acero tiene el apoyo que se ancla al vehículo. En el otro extremo se monta un pistón, que siempre se desplaza a lo largo del tubo de compresión, el cual presiona o succiona aceite que fluye a través de válvulas instaladas en el tubo de compresión.
+La función del amortiguador es controlar los movimientos de la suspensión, los muelles y/o resortes. El movimiento de la suspensión genera energía cinética, que se convierte en energía térmica o calorífica. Esta energía se disipa a través del aceite.
+</P> 
+
+
+
+<h2><li> Airbag</li></h2>
+
+<p>
+“Bolsa de aire”, en inglés. En una colisión fuerte, este cojín se infla en 30 milésimas de segundo, o menos, ante el ocupante -o a su lado, en el caso de los airbag laterales-, con el fin de evitar que se golpee con las partes rígidas del interior del coche. La bolsa (que forma parte del equipamiento de seguridad pasiva del vehículo) se vuelve a desinflar en décimas de segundo, una vez cumplida su misión amortiguadora del impacto
+</p>
+
+<h2><li> Barra estabilizadora </li></h2>
+
+<p>
+Elemento de la suspensión constituido por una barra metálica que une las dos ruedas de un mismo eje. No actúa cuando ambas ruedas se desplazan hacia arriba o hacia abajo simultáneamente. Permite tener una suspensión flexible y confortable, aumentando artificialmente la rigidez en curvas. De este modo, las barras estabilizadoras reducen el balanceo, por lo que también se las conoce como barras antibalanceo.
+</p>
+
+<h2><li> Bujia </li></h2>
+
+<p>
+Es el elemento que produce el encendido de la mezcla de combustible y aire en los cilindros, mediante una chispa, en un motor de combustión interna de encendido provocado (MEP), tanto alternativo de ciclo Otto como Wankel. 
+La bujía tiene dos funciones primarias:
+Inflamar la mezcla de aire y combustible;
+Disipar el calor generado en la cámara de combustión hacia el sistema de refrigeración del motor (rango térmico).
+</p>
+
+<h2><li> Bateria de AutoMovil </li></h2>
+
+<p>
+Es un acumulador y proporciona la energía eléctrica para el motor de arranque de un motor de combustión, como por ejemplo de un automóvil, de un alternador del motor o de la turbina de gas de un avión. Las baterías que se usan como fuente de energía para la tracción de un vehículo eléctrico se les denomina baterías de tracción. Los vehículos híbridos pueden utilizar cualquiera de los dos tipos de baterías.
+</p>
+
+<h2><li> Cambio Secuencial</li></h2>
+
+<p>
+Se denomina secuencial al cambio manual con el que, en lugar de seleccionar las marchas dibujando con la palanca las tradicionales “H”, basta mover la palanca en un solo plano, hacia delante y hacia atrás. También comprende a los cambios manuales accionados por manetas sobre el volante, puesto que no se puede seleccionar una marcha cualquiera saltándose las otras, sino que obliga a pasar por todas las intermedias. Por extensión, también se denomina secuencial a ese manejo por pulsaciones en la palanca o en botones situados en el volante en el modo manual de los cambios automáticos.
+</p>
+
+
+<h2><li> Cadena de Transmision</li></h2>
+<p>
+ sirve para transmitir del movimiento de arrastre de fuerza entre ruedas de entradas.
+Transmitir el movimiento de los pedales a la rueda en las bicicletas o del cambio a la rueda trasera en las motos.
+En los motores de 4 tiempos, para transmitir movimiento de un mecanismo a otro. Por ejemplo del cigüeñal al árbol de levas, o del cigüeñal a la bomba de lubricación del motor.
+</p>
+
+<h2><li> Catalizador</li></h2>
+
+<p>
+Elemento que, situado en el interior del tubo de escape, reacondiciona los gases producidos en la combustión. En el catalizador, los gases se recombinan químicamente y salen a la atmósfera otros menos nocivos, como si la combustión hubiese sido perfecta. En el caso de motores de gasolina, ningún modelo conseguiría la homologación sin este elemento. De este modo, se trata de un elemento obligatorio en todos los coches de gasolina. 
+</p>
+
+<h2><li> Carburador</li></h2>
+<p>
+El carburador se mezcla aire con gasolina pulverizada. La bomba de gasolina impulsa el combustible desde el depósito hasta el carburador, donde se pulveriza mediante un difusor. El pedal del acelerador controla la cantidad de mezcla que pasa a los cilindros, mientras que los diversos dispositivos del carburador regulan automáticamente la riqueza de la mezcla, esto es, la proporción de gasolina con respecto al aire
+</p>
+
+<h2><li> Culata </li></h2>
+<p>
+Culata, tapa de cilindros, cabeza del motor o tapa del bloque de cilindros es la parte superior de un motor de combustión interna que permite el cierre de las cámaras de combustión.
+</p>
+
+<h2><li> Distribuidor (automóvil) </li></h2>
+<p>
+Es un elemento del sistema de encendido en los motores de ciclo Otto (motores de gasolina, etanol y gas) que envía la corriente eléctrica de alto voltaje, procedente de la bobina de encendido, mediante un dedo o rotor giratorio en el orden requerido por el ciclo de encendido de cada uno de los cilindros hasta las bujías de cada uno de ellos.
+</p>
+
+<h2><li> Encendido electrónico </li></h2>
+<p>
+Es un sistema de encendido para motores de ciclo Otto tanto de dos tiempos (2T) como cuatro tiempos (4T) en el cual la función de interrumpir la corriente del primario de la bobina para generar por autoinducción la alta tensión necesaria en la bujía no se hace por medios mecánicos como en el sistema de ruptor o platinos, sino mediante uno o varios transistores.
+</p>
+
+<h2><li> Encendido del motor </li></h2>
+<p>
+Encendido del motor es un sistema de producción y distribución, en el caso de más de un cilindro, de la chispa de alta tensión necesaria en la bujía para producir el encendido provocado en los motores de gasolina.
+</p>
+
+<h2><li> Embrague </li></h2>
+<p>
+Es un sistema que permite tanto transmitir como interrumpir la transmisión de una energía mecánica a su acción final de manera voluntaria. En un automóvil, por ejemplo, permite al conductor controlar la transmisión del par motor desde el motor hacia las ruedas.
+</p>
+
+<h2><li> Espejo retrovisor </li></h2>
+<p>
+Es un tipo funcional de espejo que poseen los automóviles y otros vehículos, que están diseñados para permitirle al conductor ver el área que se encuentra detrás del vehículo a través de la ventana posterior.
+
+Los espejos retrovisores a veces son confundidos con los espejos laterales, que son un tipo diferente de espejos que se ubican sobre los laterales izquierdo y derecho de muchos vehículos modernos
+</p>
+
+
+<h2><li> Filtro de aire </li></h2>
+<p>
+Es un dispositivo que elimina partículas sólidas como por ejemplo polvo, polen y bacterias del aire. Los filtros de aire encuentran una utilidad allí donde la calidad del aire es de relevancia, especialmente en sistemas de ventilación de edificios y en motores tales como los de combustión interna, compresores de gas, compresores para bombonas de aire, turbinas de gas y demás.
+</p>
+
+<h2><li> Freno de disco </li></h2>
+<p>
+Es un sistema de frenado usado normalmente para ruedas de vehículos, en el cual una parte móvil (el disco) solidario con la rueda que gira es sometido al rozamiento de unas superficies de alto coeficiente de fricción (las pastillas) que ejercen sobre ellos una fuerza suficiente como para transformar toda o parte de la energía cinética del vehículo en movimiento, en calor, hasta detenerlo o reducir su velocidad, según sea el caso
+</p>
+
+<h2><li> Freno eléctrico </li></h2>
+<p>
+Es un dispositivo que permite desacelerar o detener un vehículo mediante accionamiento eléctrico. El más utilizado es el freno eléctrico "ralentizador", que se emplea en los camiones y vehículos pesados para el descenso de pendientes largas sin fatigar los frenos principales del vehículo.
+</p>
+
+<h2><li> Guantera </li></h2>
+<p>
+Una guantera es un compartimento cerrado en el automóvil (aunque también puede encontrarse en algunas motos, en otros vehículos y en algunos aviones ligeros) para almacenar objetos de primera necesidad, como pueden ser documentos, herramientas, discos compactos, etc. Suele situarse debajo del salpicadero, en el lado del copiloto. El nombre deriva del uso original del compartimento: guardar los guantes.
+</p>
+
+<h2><li>Limpiaparabrisas</li></h2>
+<p>
+Frecuentemente citado de forma incorrecta como 'parabrisas', es un dispositivo utilizado para barrer la lluvia y basura del parabrisas de un vehículo o medio de locomoción.
+</p>
+
+<h2><li> Líquido de frenos </li></h2>
+<p>
+Es un líquido hidráulico que hace posible la transmisión de la fuerza ejercida sobre el pedal de freno a los cilindros de freno en las ruedas de automóviles, motocicletas, camionetas y algunas bicicletas avanzadas.
+</p>
+
+<h2><li> Llanta </li></h2>
+<p>
+Es la pieza, normalmente metálica, sobre la que se asienta un neumático y que forma parte de la rueda, compuesta esta última por neumático, llanta y disco. En la mayoría de los países de habla hispana como México, Guatemala, El Salvador, Honduras, Venezuela, Panamá, Costa Rica, Colombia entre otros, se sigue llamando "llanta" (de llanta neumática) al neumático y "rin" o "aro" a la llanta, de modo que en esos países la rueda estaría formada por llanta y aro o rin y disco.
+</p>
+
+<h2><li>  Odómetro</li></h2>
+
+<p>
+Es un instrumento de medición que calcula la distancia total o parcial recorrida por un cuerpo (generalmente por un vehículo) en la unidad de longitud en la cual ha sido configurado (metros, millas ). Su uso está generalizadamente extendido debido a la necesidad de conocer distancias, calcular tiempos de viaje, o consumo de combustible.
+</p>
+
+<h2><li> Sistema de escape (motor) </li></h2>
+<p>
+En un equipo o vehículo que use un motor de combustión interna, es el conjunto de tubos o conductos que permiten a los gases de la combustión del motor escapar al medio ambiente.
+</p>
+
+
+<h2><li> Tacómetro </li></h2>
+<p>
+Es un dispositivo que mide la velocidad de giro de un eje, normalmente la velocidad de giro de un motor. Se mide en revoluciones por minuto (RPM). Actualmente se utilizan con mayor frecuencia los tacómetros digitales, por su mayor precisión.
+</p>
+
+<h2><li> Velocímetro  </li></h2>
+<p>
+Es un instrumento que mide el valor de la rapidez promedio de un vehículo. Debido a que el intervalo en el que mide esta rapidez es generalmente muy pequeña se aproxima mucho a la magnitud es decir la rapidez instantánea.
+</p>
+
 
 
 </body>
