47486474f8a1f5bd47409003f2ee884cdbf176b4
edekurt90 <edekurt90@gmail.com>
2016-03-08 16:49:19 -0600

    correo
    :dash:

--- application/controllers/Conctactos.php
+++ application/controllers/Conctactos.php
@@ -25,8 +25,11 @@ function __construct(){
 			$correo->asunto 	= $_POST["asunto"];
 			$correo->mensaje 	= $_POST["mensaje"];
 			$correo->imagen 	= "";
+			$correo->headers = 'From: '.$email_from."\r\n".
+			'Reply-To: '.$email_from."\r\n" .
+			'X-Mailer: PHP/' . phpversion();
 			$this->model->EnviarCorreo($correo);
-			//mail($corre->correo,$correo->asunto)
+			mail($corre->correo,$correo->asunto,$corre->headers)
 			/*if(isset($_POST["nombre"])){	
 				//$nombre=
 				$correo=
