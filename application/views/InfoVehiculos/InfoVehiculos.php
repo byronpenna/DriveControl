9f31e5100313d7dea72c0495366d7588faff5d9d
liztoloza <lissette-ct20@hotmail.com>
2016-03-28 02:43:33 -0300

    c:

---
+++ application/views/InfoVehiculos/InfoVehiculos.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php
+    // estilos universales
+  $this->load->view("parts/estilos.php");
+  ?>
+
+
+</head>
+<body>
+
+    <?php echo $VerVehiculos ?> 
+
+<div align='center'>  
+  <table border='1' cellpadding='0' cellspacing='0' width='600' bgcolor='#F6F6F6' bordercolor='#FFFFFF'>  
+    <tr>  
+      <td width='150' style='font-weight: bold'>ID</td>  
+      <td width='150' style='font-weight: bold'>motor</td>  
+      <td width='150' style='font-weight: bold'>chasis</td>  
+      <td width='150' style='font-weight: bold'></td>  
+</tr>
+
+   </table>  
+</div> 
+
+
+	<?php
+$this->load->view("parts/scripts.php");
+?>
+</body>
+</html>
