63661cbbe60432a6c8152b464e7ba7f7b2c39c95
edekurt90 <edekurt90@gmail.com>
2016-04-10 18:33:06 -0600

    ffasdfaf
    asdfa

---
+++ application/controllers/NuevaBusquedas.php
@@ -0,0 +1,20 @@
+<?php /**
+* 
+*/
+class NuevaBusquedas extends CI_Controller
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+	}
+	public function NuevaBusqueda()
+	{
+		if(isset($_SESSION["usuario"])){
+			$this->load->view("menuUsuario/NuveBusqueda.php");
+		}else{
+			redirect("/welcome/index","refresh");
+		}
+		$usuario=$_SESSION["usuario"];		
+	}
+}
