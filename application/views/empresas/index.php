83021be46062066680466cd8696c144e884df736
JennS10 <jennserrano_10@hotmail.com>
2016-04-18 01:19:02 -0600

    modificacion de empresas NO COMPLETADO

--- application/views/empresas/index.php
+++ application/views/empresas/index.php
@@ -1,7 +1,5 @@
  <!DOCTYPE html>
- <center> 
- <img src=<?php echo base_url("Content/img/banner.jpg") ?>>
- </center> 
+ 
 <html>
   
 <head>
@@ -16,10 +14,13 @@
     <link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/css/empresas/style.css") ?>>
     <link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/css/empresas/media.css") ?>>
 
-
-
 <center> 
  <img src=<?php echo base_url("Content/img/empresas.jpg") ?>>
  </center> 
+
+
+ <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left"><span class="glyphicon glyphicon-chevron-up"></span></a>
+	    
+    
 
     
